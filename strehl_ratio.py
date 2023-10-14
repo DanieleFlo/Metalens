6ff19d5fcaf5b9e8241b005eedc0f9d3be93c997
DanieleFlo <daniele.florio00@gmail.com>
2023-10-14 14:38:44 +0200

    up

--- strehl_ratio.py
+++ strehl_ratio.py
@@ -18,17 +18,17 @@ from funzioni import *
 
 img_avrg = []
 img_norm = []
-centroX = 790
-centroY = 581
 show = True  # Mostra l'immagine con il punto di massimo trovato in rosso
 saveData = True  # Se su True salva i dati
 pxToUm = 7.55e-8
 lamb = 6.33e-7
-diameter = .006
+diameter = .01
 fuoco = .05
-R = 100  # Raggio dello spot del fuoco
-avrgBreak = 1000
-Size = 350
+Imin = 0
+errore = 3
+
+Size = int(input('Dimensione area di ricerca (px): '))
+avrgBreak = int(input('Numero di immagini da considerare: '))
 
 
 def media_img():
@@ -66,7 +66,7 @@ def actual(data, vol_airy):
     #             N = N + 1
     # minData = minData/N
 
-    print('Min(V):', minData)
+    print('Minimo(V):\t', minData)
 
     img_norm = dataTemp-minData
     # img_norm = np.zeros((h, w), dtype=np.float32)
@@ -76,7 +76,7 @@ def actual(data, vol_airy):
     # # img_norm = (dataTemp-minData)
     vol_data = float(np.sum(img_norm))
     img_norm = img_norm*vol_airy/vol_data
-    print('Vol dati:', round(vol_data, 2))
+    print('Vol dati:\t', round(vol_data, 2))
     return [np.max(img_norm), img_norm]
 
 
@@ -106,8 +106,8 @@ def actual_profile(data, area_teoY, area_teoX):
     minY = np.min(img_normY)
     img_normX = (img_normX-minX)*area_teoX/areaX
     img_normY = (img_normY-minY)*area_teoY/areaY
-    print('Min(X):', minX, '\nMin(Y):', minY)
-    print('Vol dati(X):', areaX, '\nVol dati(Y):', areaY)
+    print('Minimo(X):\t', minX, '\nMinimo(Y):\t', minY)
+    print('Vol dati(X):\t', areaX, '\nVol dati(Y):\t', areaY)
     return [np.max(img_normY), np.max(img_normX), img_normX, img_normY]
 
 
@@ -157,77 +157,110 @@ def ideal_profile(img_airy):
     return [profX, profY, areaY, areaX]
 
 
+print('\n-----Inizio calcolo media sulla immagini-----')
 img_avrg = media_img()
-print()
-print('-----Inizio calcolo parametri-----')
+
+
+# Cerco se ci sono punti di massimo
+print('\n-----Inizio ricerca massimo-----')
+maxP = []
+puntiMax = pMax(img_avrg, Imin, errore)
+if len(puntiMax) > 1:
+    print('Ho trovato ' +
+          str(len(puntiMax)) + ' punti di massimo')
+    print('Ricerca massimo: 0%')
+    r = round(math.sqrt(len(puntiMax)/6.2931)/2)
+    if r < 10:
+        r = 10
+    pMaxFiltrati = filtra_vicinato(
+        puntiMax, img_avrg, r, len(puntiMax), 'Img media ')
+    if len(pMaxFiltrati) == 1:
+        maxP = pMaxFiltrati[0]
+    else:
+        print('Errore nella ricerca del massimo')
+else:
+    if len(puntiMax) == 1:
+        print('Ho trovato ' +
+              str(len(puntiMax)) + ' punto di massimo')
+        print('Ricerca massimo: 0%')
+        maxP = puntiMax[0]
+        print('Ricerca massimo: 100%')
+    else:
+        print('Non ho trovato massimi')
+CxMax = maxP[2]
+CyMax = maxP[1]
+print('\n-----Inizio calcolo parametri-----')
 dataTemp = np.array(img_avrg)
 h, w = dataTemp.shape
 
 img_avrg_small = np.zeros((Size, Size), dtype=np.float32)
 for y in range(Size):
     for x in range(Size):
-        Y = centroY-y+round(Size/2)
-        X = centroX-x+round(Size/2)
+        Y = CyMax-y+round(Size/2)
+        X = CxMax-x+round(Size/2)
         img_avrg_small[y, x] = img_avrg[Y, X]
 
 vol_airy, img_airy = ideal(img_avrg_small)
 
-print('Vol airy:',  round(vol_airy))
+print('Vol airy:\t',  round(vol_airy))
 
 img_airyX, img_airyY, areaY, areaX = ideal_profile(img_airy)
 
-print('Area teorica (Y):', round(areaY, 2),
-      '\nArea teorica (X):', round(areaX, 2))
+print('Area teo(Y):\t', round(areaY, 2),
+      '\nArea teo(X)):\t', round(areaX, 2))
 
 cp, img_norm = actual(img_avrg_small, vol_airy)
 cpX, cpY, profX, profY = actual_profile(img_avrg_small, areaY, areaX)
 
 
-print('S. ratio(V):', round(cp, 3), '\nS. ratio(X):',
-      round(cpX, 3), '\nS. ratio(Y):', round(cpY, 3))
+print('S. ratio(V):\t', round(cp, 3), '\nS. ratio(X):\t',
+      round(cpX, 3), '\nS. ratio(Y):\t', round(cpY, 3))
 
 
 if saveData == True:
-    print('Salvo i dati')
-    profileX = profile_dataX(img_avrg, centroY)
-    profileY = profile_dataY(img_avrg, centroX)
+    print('\nSalvo i dati')
+    profileX = profile_dataX(img_avrg, CyMax)
+    profileY = profile_dataY(img_avrg, CxMax)
+    midS = round(Size/2)
     with open('result/profile_fuoco_average_X.dat', 'w') as file:
         file.write('X\tI\n')
         for i in range(len(profileX)):
-            file.write(str(i) + '\t' + str(profileX[i][0]) + '\n')
+            file.write(str((i-midS)*pxToUm) + '\t' +
+                       str(profileX[i][0]) + '\n')
 
     with open('result/profile_fuoco_average_Y.dat', 'w') as file:
         file.write('Y\tI\n')
         for i in range(len(profileY)):
-            file.write(str(i) + '\t' + str(profileY[i][0]) + '\n')
+            file.write(str((i-midS)*pxToUm) + '\t' +
+                       str(profileY[i][0]) + '\n')
 
     with open('result/profile_norm_Y.dat', 'w') as file:
         file.write('Y\tI\n')
         for i in range(len(profY)):
-            file.write(str(i) + '\t' + str(profY[i]) + '\n')
+            file.write(str((i-midS)*pxToUm) + '\t' + str(profY[i]) + '\n')
 
     with open('result/profile_norm_X.dat', 'w') as file:
         file.write('X\tI\n')
         for i in range(len(profX)):
-            file.write(str(i) + '\t' + str(profX[i]) + '\n')
+            file.write(str((i-midS)*pxToUm) + '\t' + str(profX[i]) + '\n')
 
     with open('result/profile_teo_X.dat', 'w') as file:
         file.write('X\tI\n')
         for i in range(len(img_airyX)):
-            file.write(str(i) + '\t' + str(img_airyX[i]) + '\n')
+            file.write(str((i-midS)*pxToUm) + '\t' + str(img_airyX[i]) + '\n')
 
     with open('result/profile_teo_Y.dat', 'w') as file:
         file.write('Y\tI\n')
         for i in range(len(img_airyY)):
-            file.write(str(i) + '\t' + str(img_airyY[i]) + '\n')
+            file.write(str((i-midS)*pxToUm) + '\t' + str(img_airyY[i]) + '\n')
 
     with open('result/parametri.dat', 'w') as file:
-        file.write('S. ratio(V):' + str(cp) + '\nS. ratio(X):' +
-                   str(cpX) + '\nS. ratio(Y):' + str(cpY))
-        file.write('\nDiametro(m):' + str(diameter) + '\npxToUm(m/px):' +
-                   str(pxToUm) + '\nlamb(m):' + str(lamb) + '\nfuoco(m):' + str(fuoco))
-        file.write('\ncentroX(px):' + str(centroX) +
-                   '\ncentroY(px):' + str(centroY))
+        file.write('Strehl ratio:\n' + 'S. ratio(V):\t' + str(cp) + '\nS. ratio(X):\t' +
+                   str(cpX) + '\nS. ratio(Y):\t' + str(cpY) + '\n')
+        file.write('\nInfo:' + '\nDiametro(m):\t' + str(diameter) + '\npxToUm(m/px):\t' +
+                   str(pxToUm) + '\nL. d\'onda(m):\t' + str(lamb) + '\nFuoco(m):\t' + str(fuoco))
+        file.write('\nCentro X(px):\t' + str(CxMax) +
+                   '\nCentro Y(px):\t' + str(CyMax))
 
 if show == True:
     print('Mostro i dati')
@@ -276,7 +309,7 @@ if show == True:
     axs[5].set_xlabel('y(m)')
     axs[5].set_ylabel('I(au)')
     axs[5].title.set_text('Profilo fuoco lungo Y, atteso')
-    
+
     plt.show()
 
 print('-----Fine-----')
