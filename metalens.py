ad533e95f94473d1e70be4ebbe79ad2cc5a67a48
DanieleFlo <daniele.florio00@gmail.com>
2023-11-02 18:28:28 +0100

    up

--- metalens.py
+++ metalens.py
@@ -2,24 +2,26 @@
 import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
+import math
 
 import tidy3d as td
 from tidy3d import web
+
+start_sim = True
 
 # 1 nanometer in units of microns (for conversion)
 nm = 1e-3
 
 # free space central wavelength
-wavelength = 600 * nm
+wavelength = 633 * nm
 
 # desired numerical aperture
-NA = 0.5
+NA = 0.2
 
 # shape parameters of metalens unit cell (um) (refer to image above and see paper for details)
-W = 85 * nm
-L = 410 * nm
-H = 600 * nm
-S = 430 * nm
+W = 200 * nm
+H = 1500 * nm  # Altezza del pillars
+S = 430 * nm  # Dimensione della cella
 
 # space between bottom PML and substrate (-z)
 space_below_sub = 1 * wavelength
@@ -28,12 +30,13 @@ space_below_sub = 1 * wavelength
 thickness_sub = 100 * nm
 
 # side length of entire metalens (um)
-side_length = 10
+side_length = 20
 
 # Number of unit cells in each x and y direction (NxN grid)
 N = int(side_length / S)
 print(f"for diameter of {side_length:.1f} um, have {N} cells per side")
-print(f"full metalens has area of {side_length**2:.1f} um^2 and {N*N} total cells")
+print(
+    f"full metalens has area of {side_length**2:.1f} um^2 and {N*N} total cells")
 
 # Define material properties at 600 nm
 n_TiO2 = 2.40
@@ -51,11 +54,6 @@ length_xy = N * S
 
 # focal length given diameter and numerical aperture
 f = length_xy / 2 / NA * np.sqrt(1 - NA**2)
-
-# Function describing the theoretical best angle of each box at position (x,y).  see paper for details
-def theta(x, y):
-    return np.pi / wavelength * (f - np.sqrt(x**2 + y**2 + f**2))
-
 
 # total domain size in z: (space -> substrate -> unit cell -> 1.7 focal lengths)
 length_z = space_below_sub + thickness_sub + H + 1.7 * f
@@ -77,42 +75,29 @@ centers_x = S * np.arange(N) - length_xy / 2.0 + S / 2.0
 centers_y = S * np.arange(N) - length_xy / 2.0 + S / 2.0
 center_z = -length_z / 2 + space_below_sub + thickness_sub + H / 2.0
 
-# x, y vertices of box of size (L, W) centered at the origin
-vertices_origin = np.array(
-    [[+L / 2, +W / 2], [-L / 2, +W / 2], [-L / 2, -W / 2], [+L / 2, -W / 2]]
-)
-
-
-xs, ys = np.meshgrid(centers_x, centers_y, indexing="ij")
-xs = xs.flatten()
-ys = ys.flatten()
-
-angles = theta(xs, ys)
-
-# 2x2 rotation matrix angle `angle` with respect to x axis
-rotation_matrix = np.array(
-    [[+np.cos(angles), -np.sin(angles)], [+np.sin(angles), +np.cos(angles)]]
-)
-
-# rotate the origin vertices by this angle
-vertices_rotated = np.einsum("ij, jkn -> nik", vertices_origin, rotation_matrix)
-
-# shift the rotated vertices to be centered at (xs, ys)
-vertices_shifted = vertices_rotated + np.stack([xs, ys], axis=-1)[:, None, :]
-
 metalens_geometry = []
-for vertices in vertices_shifted:
-    # create a tidy3D PolySlab with these rotated and shifted vertices and thickness `H`
-    metalens_geometry.append(
-        td.PolySlab(
-            vertices=vertices.tolist(),
-            slab_bounds=(center_z - H / 2, center_z + H / 2),
-            axis=2,
-        ),
-    )
+
+for x in centers_x:
+    for y in centers_y:
+        dis = math.sqrt(x**2+y**2)
+        phi = (2*math.pi/wavelength) * \
+            (math.sqrt(x**2+y**2+f**2)-f) % (2*math.pi)
+        alpha = W/(2*math.pi)
+        r = W-(phi*alpha)
+        # print(dis, length_xy)
+        # print('W:', W, '; phi:', phi, '; r', r)
+        if r > 0 and dis <= length_xy/2:
+            metalens_geometry.append(
+                td.Cylinder(
+                    axis=2,
+                    center=(x, y, center_z),
+                    radius=r,
+                    length=H,
+                )
+            )
 
 metalens = td.Structure(
-    geometry=td.GeometryGroup(geometries=metalens_geometry), medium=TiO2
+    geometry=td.GeometryGroup(geometries=metalens_geometry), medium=SiO2
 )
 
 # steps per unit cell along x and y
@@ -139,7 +124,7 @@ grid_spec = grid_spec.copy(
                     rmin=(-td.inf, -td.inf, -length_z / 2 + space_below_sub),
                     rmax=(td.inf, td.inf, center_z + H / 2),
                 ),
-                medium=TiO2,
+                medium=SiO2,
             )
         ]
     )
@@ -167,7 +152,8 @@ fields = ["Ex", "Ey", "Ez"]
 
 # get fields along x=y=0 axis
 monitor_center = td.FieldMonitor(
-    center=[0.0, 0.0, 0], size=[0, 0, td.inf], freqs=[f0], name="center"
+    center=[0.0, 0.0, 0], size=[0, 0, td.inf], freqs=[f0], name="center",
+    colocate=True,
 )
 
 # get the fields at a few cross-sectional planes
@@ -177,6 +163,7 @@ monitor_xz = td.FieldMonitor(
     freqs=[f0],
     name="xz",
     fields=fields,
+    colocate=True,
 )
 
 monitor_yz = td.FieldMonitor(
@@ -185,6 +172,7 @@ monitor_yz = td.FieldMonitor(
     freqs=[f0],
     name="yz",
     fields=fields,
+    colocate=True,
 )
 
 monitor_xy = td.FieldMonitor(
@@ -193,168 +181,88 @@ monitor_xy = td.FieldMonitor(
     freqs=[f0],
     name="focal_plane",
     fields=fields,
+    colocate=True,
 )
 
 # put them into a single list
 monitors = [monitor_center, monitor_xz, monitor_yz, monitor_xy]
 
+if start_sim == True:
+    sim = td.Simulation(
+        size=sim_size,
+        #grid_spec=grid_spec,
+        structures=[substrate, metalens],
+        sources=[source],
+        monitors=monitors,
+        run_time=run_time,
+        boundary_spec=td.BoundarySpec(
+            x=td.Boundary.absorber(), y=td.Boundary.absorber(), z=td.Boundary.pml()
+        ),
+    )
 
-sim = td.Simulation(
-    size=sim_size,
-    grid_spec=grid_spec,
-    structures=[substrate, metalens],
-    sources=[source],
-    monitors=monitors,
-    run_time=run_time,
-    boundary_spec=td.BoundarySpec(
-        x=td.Boundary.absorber(), y=td.Boundary.absorber(), z=td.Boundary.pml()
-    ),
-)
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 6))
 
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 6))
+    sim.plot(x=0.1, ax=ax1)
+    sim.plot(y=0.1, ax=ax2)
+    sim.plot(z=-length_z / 2 + space_below_sub + thickness_sub + H / 2, ax=ax3)
+    plt.title("Strutture")
+    plt.show()
 
-sim.plot(x=0.1, ax=ax1)
-sim.plot(y=0.1, ax=ax2)
-sim.plot(z=-length_z / 2 + space_below_sub + thickness_sub + H / 2, ax=ax3)
-plt.show()
+    # sim.plot_3d()
 
+    job = web.Job(simulation=sim, task_name="metalens", verbose=True)
+    sim_data = job.run(path="data/simulation_data.hdf5")
 
-#sim.plot_3d()
+    print(sim_data.log)
 
+    focal_z = center_z + H / 2 + f
+    data_center_line = sim_data["center"]
+    I = (
+        abs(data_center_line.Ex) ** 2
+        + abs(data_center_line.Ey) ** 2
+        + abs(data_center_line.Ez) ** 2
+    )
+    I.plot()
+    plt.title("intensity(z)")
+    plt.show()
 
-job = web.Job(simulation=sim, task_name="metalens", verbose=True)
-sim_data = job.run(path="data/simulation_data.hdf5")
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4))
+    sim_data.plot_field("xz", "Ex", val="real", f=f0,
+                        y=0, ax=ax1, vmin=-15, vmax=15)
+    sim_data.plot_field("yz", "Ex", val="real", f=f0,
+                        x=0, ax=ax2, vmin=-15, vmax=15)
+    sim_data.plot_field("focal_plane", "Ex", val="real", f=f0,
+                        z=focal_z, ax=ax3, vmin=-15, vmax=15)
+    ax1.set_title("x-z plane")
+    ax2.set_title("y-z plane")
+    ax3.set_title("x-y (focal) plane")
+    plt.title("Parte reale")
+    plt.show(block=False)
 
-print(sim_data.log)
+    # plot absolute value for good measure
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4))
+    sim_data.plot_field("xz", "Ex", val="abs", f=f0,
+                        y=0, ax=ax1, vmin=0, vmax=15)
+    sim_data.plot_field("yz", "Ex", val="abs", f=f0,
+                        x=0, ax=ax2, vmin=0, vmax=15)
+    sim_data.plot_field("focal_plane", "Ex", val="abs", f=f0,
+                        z=focal_z, ax=ax3, vmin=0, vmax=15)
+    ax1.set_title("x-z plane")
+    ax2.set_title("y-z plane")
+    ax3.set_title("x-y (focal) plane")
+    plt.title("Abs")
+    plt.show(block=False)
 
-focal_z = center_z + H / 2 + f
-data_center_line = sim_data["center"]
-I = (
-    abs(data_center_line.Ex) ** 2
-    + abs(data_center_line.Ey) ** 2
-    + abs(data_center_line.Ez) ** 2
-)
-I.plot()
-plt.title("intensity(z)")
-plt.show()
-
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4))
-sim_data.plot_field("xz", "Ex", val="real", f=f0, y=0, ax=ax1, vmin=-15, vmax=15)
-sim_data.plot_field("yz", "Ex", val="real", f=f0, x=0, ax=ax2, vmin=-15, vmax=15)
-sim_data.plot_field("focal_plane", "Ex", val="real", f=f0, z=focal_z, ax=ax3, vmin=-15, vmax=15)
-ax1.set_title("x-z plane")
-ax2.set_title("y-z plane")
-ax3.set_title("x-y (focal) plane")
-plt.show()
-
-# plot absolute value for good measure
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4))
-sim_data.plot_field("xz", "Ex", val="abs", f=f0, y=0, ax=ax1, vmin=0, vmax=15)
-sim_data.plot_field("yz", "Ex", val="abs", f=f0, x=0, ax=ax2, vmin=0, vmax=15)
-sim_data.plot_field("focal_plane", "Ex", val="abs", f=f0, z=focal_z, ax=ax3, vmin=0, vmax=15)
-ax1.set_title("x-z plane")
-ax2.set_title("y-z plane")
-ax3.set_title("x-y (focal) plane")
-plt.show()
-
-
-# and let's plot the intensites as well
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4))
-sim_data.plot_field("xz", "E", "abs^2", f=f0, y=0, ax=ax1, vmin=0, vmax=3e2)
-sim_data.plot_field("yz", "E", "abs^2", f=f0, x=0, ax=ax2, vmin=0, vmax=3e2)
-sim_data.plot_field("focal_plane", "E", "abs^2", f=f0, z=focal_z, ax=ax3, vmin=0, vmax=3e2)
-ax1.set_title("x-z plane")
-ax2.set_title("y-z plane")
-ax3.set_title("x-y (focal) plane")
-plt.show()
-
-# create a field projection monitor in Cartesian coordinates which records near fields just above the strucure,
-# and projects them to points on the focal plane
-
-# number of focal plane sampling points in the x and y directions
-num_far = 200
-
-# define the focal plane sample points at which to project fields
-xs_far = np.linspace(-sim_size[0] / 2, sim_size[0] / 2, num_far)
-ys_far = np.linspace(-sim_size[1] / 2, sim_size[1] / 2, num_far)
-
-pos_monitor_z = -6
-proj_distance = monitor_xy.center[2] - pos_monitor_z
-monitor_proj = td.FieldProjectionCartesianMonitor(
-    center=[0.0, 0.0, pos_monitor_z],
-    size=[td.inf, td.inf, 0],
-    freqs=[f0],
-    name="focal_plane_proj",
-    proj_axis=2,  # axis along which to project, in this case z
-    proj_distance=proj_distance,  # distance from this monitor to where fields are projected
-    x=xs_far,
-    y=ys_far,
-    custom_origin=[0.0, 0.0, pos_monitor_z],
-    far_field_approx=False,  # the distance to the focal plane is comparable to the size of the structure, so
-    # turn off the far-field approximation and use an exact Green's function to
-    # project the fields
-)
-
-# create a simulation as before, but this time there's no need to include the large amount of
-# empty space up to the focal plane, and include the projection monitor
-
-# total domain size in z: (space -> substrate -> unit cell -> space)
-length_z_new = space_below_sub + thickness_sub + 2 * H + space_below_sub
-sim_size = (length_xy, length_xy, length_z_new)
-sim_center = (0, 0, -length_z / 2 + length_z_new / 2)
-sim_new = td.Simulation(
-    size=sim_size,
-    center=sim_center,
-    grid_spec=grid_spec,
-    structures=[substrate, metalens],
-    sources=[source],
-    monitors=[monitor_proj],
-    run_time=run_time,
-    boundary_spec=td.BoundarySpec(
-        x=td.Boundary.absorber(), y=td.Boundary.absorber(), z=td.Boundary.pml()
-    ),
-)
-
-# visualize to make sure everything looks okay
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 6))
-sim_new.plot(x=0.1, ax=ax1)
-sim_new.plot(y=0.1, ax=ax2)
-sim_new.plot(z=-length_z / 2 + space_below_sub + thickness_sub + H / 2, ax=ax3)
-plt.show()
-
-job = web.Job(simulation=sim_new, task_name="metalens", verbose=True)
-sim_data_new = job.run(path="data/simulation_data_new.hdf5")
-print(sim_data_new.log)
-
-# plot the focal plane electric field components recorded by directly placing a monitor in the first simulation,
-# which we can use as a reference
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14.3, 3.6))
-focal_field_data = sim_data["focal_plane"]
-Ex = focal_field_data.Ex.sel(f=f0, z=focal_z)
-Ey = focal_field_data.Ey.sel(f=f0, z=focal_z)
-Ez = focal_field_data.Ez.sel(f=f0, z=focal_z)
-im1 = ax1.pcolormesh(Ex.y, Ex.x, np.real(Ex), cmap="RdBu", shading="auto")
-im2 = ax2.pcolormesh(Ey.y, Ey.x, np.real(Ey), cmap="RdBu", shading="auto")
-im3 = ax3.pcolormesh(Ez.y, Ez.x, np.real(Ez), cmap="RdBu", shading="auto")
-fig.colorbar(im1, ax=ax1)
-fig.colorbar(im2, ax=ax2)
-fig.colorbar(im3, ax=ax3)
-
-# now plot the projected fields computed via the second simulation
-proj_fields = sim_data_new["focal_plane_proj"].fields_cartesian.sel(
-    f=f0, z=monitor_proj.proj_distance
-)
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14.3, 3.6))
-im1 = ax1.pcolormesh(
-    ys_far, xs_far, np.real(proj_fields.Ex), cmap="RdBu", shading="auto"
-)
-im2 = ax2.pcolormesh(
-    ys_far, xs_far, np.real(proj_fields.Ey), cmap="RdBu", shading="auto"
-)
-im3 = ax3.pcolormesh(
-    ys_far, xs_far, np.real(proj_fields.Ez), cmap="RdBu", shading="auto"
-)
-fig.colorbar(im1, ax=ax1)
-fig.colorbar(im2, ax=ax2)
-fig.colorbar(im3, ax=ax3)
-plt.show()
+    # and let's plot the intensites as well
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4))
+    sim_data.plot_field("xz", "E", "abs^2", f=f0,
+                        y=0, ax=ax1, vmin=0, vmax=3e2)
+    sim_data.plot_field("yz", "E", "abs^2", f=f0,
+                        x=0, ax=ax2, vmin=0, vmax=3e2)
+    sim_data.plot_field("focal_plane", "E", "abs^2", f=f0,
+                        z=focal_z, ax=ax3, vmin=0, vmax=3e2)
+    ax1.set_title("x-z plane")
+    ax2.set_title("y-z plane")
+    ax3.set_title("x-y (focal) plane")
+    plt.title("Abs^2")
+    plt.show()
